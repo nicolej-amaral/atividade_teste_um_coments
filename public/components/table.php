09df777b72df73cb0f247350e7587f33c2dd6d99
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-06-02 09:39:58 -0300

    add - Iniciei os comentários da pasta table.php

--- public/components/table.php
+++ public/components/table.php
@@ -1,17 +1,24 @@
+<!-- Adicionar título tamanho "h3" a tabela .-->
 <h3>Usuários Cadastrados</h3>
 
+<!-- Criar a tabela, adicionando a ela o tamanho da borda, e do espaçamento dos itens dentro dela. -->
 <table border="1" cellpadding="3">
 
+<!-- Adicionar a tabela, as colunas "ID", "Nome" e "Senha", para o cadastro de usuários. -->
     <tr>
         <th>ID</th>
         <th>Nome</th>
         <th>Senha</th>
     </tr>
 
+
     <?php
-    
+    // Selecionar e atribuir  a tabela "users" a $sqlUsuarios.
     $sqlUsuarios = "SELECT * FROM users";
+
+    // Declara a variável " $resultadoUsuarios" e pega a tabela.
     $resultadoUsuarios = $conn->query($sqlUsuarios);
+    
     
     while($linha = $resultadoUsuarios->fetch_assoc()){
 
